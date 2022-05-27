572bd3535694e97fae5fc1fc29eed974a60a5a07
nqhoang2077-mairm1 <nqhoang2077@gmail.com>
2022-05-27 11:26:23 +0700

    solved: change_dp

--- cpp/change_dp.cpp
+++ cpp/change_dp.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int get_change(int m) {
-  //write your code here
-  return m / 4;
+using namespace std;
+
+int get_change(int m)
+{
+  // write your code here;
+  vector<int> changes{0, 1, 2, 1, 1};
+  changes.resize(m + 1);
+  if (m < 5)
+    return changes[m];
+  for (int i = 5; i < m + 1; i++)
+  {
+    int change1 = changes[i-1] + changes[1];
+    int change3 = changes[i-3] + changes[3];
+    int change4 = changes[i-4] + changes[4];
+    changes[i] = min(change1, min(change3, change4));
+  }
+  // for_each(changes.begin(), changes.end(), [](int x){cout << x << ", "; });
+  return changes[m];
 }
 
-int main() {
+int main()
+{
   int m;
   std::cin >> m;
   std::cout << get_change(m) << '\n';
