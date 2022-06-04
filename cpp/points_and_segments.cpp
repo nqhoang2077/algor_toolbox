3d662290618e1dfa983d48941de59ffd7eded1b3
hoang@linux <nqhoang2077@gmail.com>
2022-06-04 08:32:30 +0700

    testing bitwise

--- cpp/points_and_segments.cpp
+++ cpp/points_and_segments.cpp
@@ -8,68 +8,111 @@ using namespace std;
 
 using segment = std::pair<int, int>;
 
-int searchPoint(segment& seg, const vector<int>& points, function<bool(int)> condition){
-  auto left {0}, right{static_cast<int>(points.size()-1)};
-  int mid = (left + right) / 2;
-  if (condition(points[mid])){
+int searchPoint(segment &seg, const vector<int> &points, int target)
+{
+  auto left{0}, right{static_cast<int>(points.size() - 1)};
+  int mid{-1};
 
-  } else {
-    
+  while (left <= right)
+  {
+    mid = (left + right) / 2;
+    if (points[mid] >= target)
+    {
+      right = mid - 1;
+    }
+    else
+    {
+      left = mid + 1;
+    }
   }
+  return left;
 }
 
-void findIncludedPoints(segment& seg, const vector<int>& points, vector<int>& cnt){
+void findIncludedPoints(segment &seg, const vector<int> &points, vector<int> &cnt)
+{
   // Find start point
-  int start = searchPoint(seg, points, [=](int value){return value >= seg.first;});
+  int start = searchPoint(seg, points, seg.first);
 
   // Find end point
-  int end = searchPoint(seg, points, [=](int value){return value <= seg.second;});
+  int end = searchPoint(seg, points, seg.second + 1) - 1;
 
   // Increase them and everything in between them by 1
   while (start < end)
     cnt[start++]++;
 }
 
-vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
+vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points)
+{
   vector<int> cnt(points.size());
-  for_each(cnt.begin(), cnt.end(),[](int& x){x=0;});
-  //write your code here
+  for_each(cnt.begin(), cnt.end(), [](int &x)
+           { x = 0; });
+  // write your code here
   vector<segment> segments(starts.size());
   std::sort(starts.begin(), starts.end());
   for (int i = 0; i < starts.size(); i++)
-    segments.push_back({starts[i],ends[i]});
+    segments.push_back({starts[i], ends[i]});
 
-  for (segment& s : segments){
+  for (segment &s : segments)
+  {
     findIncludedPoints(s, points, cnt);
   }
 
   return cnt;
 }
 
-vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
+vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<int> points)
+{
   vector<int> cnt(points.size());
-  for (size_t i = 0; i < points.size(); i++) {
-    for (size_t j = 0; j < starts.size(); j++) {
+  for (size_t i = 0; i < points.size(); i++)
+  {
+    for (size_t j = 0; j < starts.size(); j++)
+    {{}
       cnt[i] += starts[j] <= points[i] && points[i] <= ends[j];
     }
   }
   return cnt;
 }
 
-int main() {
+int main1()
+{
   int n, m;
   std::cin >> n >> m;
   vector<int> starts(n), ends(n);
-  for (size_t i = 0; i < starts.size(); i++) {
+  for (size_t i = 0; i < starts.size(); i++)
+  {
     std::cin >> starts[i] >> ends[i];
   }
   vector<int> points(m);
-  for (size_t i = 0; i < points.size(); i++) {
+  for (size_t i = 0; i < points.size(); i++)
+  {
     std::cin >> points[i];
   }
-  //use fast_count_segments
+  // use fast_count_segments
   vector<int> cnt = naive_count_segments(starts, ends, points);
-  for (size_t i = 0; i < cnt.size(); i++) {
+  for (size_t i = 0; i < cnt.size(); i++)
+  {
+    std::cout << cnt[i] << ' ';
+  }
+}
+
+int main()
+{
+  int n, m;
+  std::cin >> n >> m;
+  vector<int> starts(n), ends(n);
+  for (size_t i = 0; i < starts.size(); i++)
+  {
+    std::cin >> starts[i] >> ends[i];
+  }
+  vector<int> points(m);
+  for (size_t i = 0; i < points.size(); i++)
+  {
+    std::cin >> points[i];
+  }
+  // use fast_count_segments
+  vector<int> cnt = naive_count_segments(starts, ends, points);
+  for (size_t i = 0; i < cnt.size(); i++)
+  {
     std::cout << cnt[i] << ' ';
   }
 }
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -3,22 +3,10 @@
 
 using namespace std;
 
-void rotate(vector<int> &nums, int k)
-{
-    // k = k % nums.size();
-    auto k_pointer = nums.begin() + k;
-    vector<int> temp(k_pointer, nums.end());
-    for (auto it = nums.begin(); it < k_pointer; ++it)
-    {
-        temp.push_back(*it);
-    }
-    nums.swap(temp);
-}
-
 int main()
 {
-    vector<int> nums{1,2,3,4,5,6,7};
-    rotate(nums, 3);
-    cout << nums << endl;
+    int x = 122;
+    x = x >> 1 << 1;
+    cout << x;
     return 0;
 }
